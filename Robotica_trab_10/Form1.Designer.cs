326bb904ddb038fe8485bd35795327f02b45dad3
PedruuH <pedro.inventador@hotmail.com>
2021-06-14 19:00:10 -0300

    final

---
+++ Robotica_trab_10/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Robotica_trab_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tb_co_x = new System.Windows.Forms.TextBox();
+            this.tb_co_y = new System.Windows.Forms.TextBox();
+            this.tb_co_angulo = new System.Windows.Forms.TextBox();
+            this.lb_t1 = new System.Windows.Forms.Label();
+            this.lb_t2 = new System.Windows.Forms.Label();
+            this.lb_t3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_l1 = new System.Windows.Forms.TextBox();
+            this.tb_l2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_co_x
+            // 
+            this.tb_co_x.Location = new System.Drawing.Point(15, 29);
+            this.tb_co_x.Name = "tb_co_x";
+            this.tb_co_x.Size = new System.Drawing.Size(100, 20);
+            this.tb_co_x.TabIndex = 0;
+            this.tb_co_x.Text = "0";
+            // 
+            // tb_co_y
+            // 
+            this.tb_co_y.Location = new System.Drawing.Point(15, 73);
+            this.tb_co_y.Name = "tb_co_y";
+            this.tb_co_y.Size = new System.Drawing.Size(100, 20);
+            this.tb_co_y.TabIndex = 1;
+            this.tb_co_y.Text = "0";
+            // 
+            // tb_co_angulo
+            // 
+            this.tb_co_angulo.Location = new System.Drawing.Point(15, 115);
+            this.tb_co_angulo.Name = "tb_co_angulo";
+            this.tb_co_angulo.Size = new System.Drawing.Size(100, 20);
+            this.tb_co_angulo.TabIndex = 2;
+            this.tb_co_angulo.Text = "0";
+            // 
+            // lb_t1
+            // 
+            this.lb_t1.AutoSize = true;
+            this.lb_t1.Location = new System.Drawing.Point(212, 192);
+            this.lb_t1.Name = "lb_t1";
+            this.lb_t1.Size = new System.Drawing.Size(0, 13);
+            this.lb_t1.TabIndex = 3;
+            // 
+            // lb_t2
+            // 
+            this.lb_t2.AutoSize = true;
+            this.lb_t2.Location = new System.Drawing.Point(222, 78);
+            this.lb_t2.Name = "lb_t2";
+            this.lb_t2.Size = new System.Drawing.Size(0, 13);
+            this.lb_t2.TabIndex = 4;
+            // 
+            // lb_t3
+            // 
+            this.lb_t3.AutoSize = true;
+            this.lb_t3.Location = new System.Drawing.Point(360, 78);
+            this.lb_t3.Name = "lb_t3";
+            this.lb_t3.Size = new System.Drawing.Size(0, 13);
+            this.lb_t3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Coordenada X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Coordenada Y";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Ângulo";
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(15, 238);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(75, 23);
+            this.btn_calcular.TabIndex = 9;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(129, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(279, 239);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 138);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "L 1";
+            // 
+            // tb_l1
+            // 
+            this.tb_l1.Location = new System.Drawing.Point(15, 154);
+            this.tb_l1.Name = "tb_l1";
+            this.tb_l1.Size = new System.Drawing.Size(46, 20);
+            this.tb_l1.TabIndex = 12;
+            this.tb_l1.Text = "0";
+            // 
+            // tb_l2
+            // 
+            this.tb_l2.Location = new System.Drawing.Point(69, 154);
+            this.tb_l2.Name = "tb_l2";
+            this.tb_l2.Size = new System.Drawing.Size(46, 20);
+            this.tb_l2.TabIndex = 14;
+            this.tb_l2.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(66, 138);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "L 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 270);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tb_l2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tb_l1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lb_t3);
+            this.Controls.Add(this.lb_t2);
+            this.Controls.Add(this.lb_t1);
+            this.Controls.Add(this.tb_co_angulo);
+            this.Controls.Add(this.tb_co_y);
+            this.Controls.Add(this.tb_co_x);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_co_x;
+        private System.Windows.Forms.TextBox tb_co_y;
+        private System.Windows.Forms.TextBox tb_co_angulo;
+        private System.Windows.Forms.Label lb_t1;
+        private System.Windows.Forms.Label lb_t2;
+        private System.Windows.Forms.Label lb_t3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb_l1;
+        private System.Windows.Forms.TextBox tb_l2;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Robotica_trab_10/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Robotica_trab_10
+{
+    public partial class Form1 : Form
+    {
+        double corrd_x;
+        double corrd_y;
+        double angulo;
+        double l1, l2;
+        double teta1, teta2, teta3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            corrd_x = 0;
+            corrd_y = 0;
+            angulo = 0;
+            l1 = 0;
+            l2 = 0;
+            teta1 = teta2 = teta3 = 0;
+            set_valores();
+            calcula_tetas();
+            retorna_angulos();
+        }
+
+        void set_valores()
+        {
+            corrd_x = Double.Parse(tb_co_x.Text);
+            corrd_y = Double.Parse(tb_co_y.Text);
+            angulo = Double.Parse(tb_co_angulo.Text);
+            l1 = Double.Parse(tb_l1.Text);
+            l2 = Double.Parse(tb_l2.Text);
+
+        }
+        void set_valores_toZero()
+        {
+            corrd_x = 0;
+            corrd_y = 0;
+            angulo = 0;
+            l1 = 0;
+            l2 = 0;
+            teta1 = teta2 = teta3 = 0;
+            tb_co_x.Text = corrd_x.ToString();
+            tb_co_y.Text = corrd_y.ToString();
+            tb_co_angulo.Text = angulo.ToString();
+            tb_l1.Text = l1.ToString();
+            tb_l2.Text = l2.ToString();
+            retorna_angulos();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            set_valores_toZero();
+        }
+
+        void calcula_tetas()
+        {
+            teta2 = 180 * ((double)Math.Acos((Math.Pow(corrd_x, 2) + Math.Pow(corrd_y, 2) - Math.Pow(l1, 2) - Math.Pow(l2, 2)) / (2 * l1 * l2))) / Math.PI;
+                        
+            if (teta2 < 0)
+            {
+                teta1 = 180*( (double) Math.Atan2(corrd_y, corrd_x) + Math.Acos((Math.Pow(corrd_x, 2) + Math.Pow(corrd_y, 2) + Math.Pow(l1, 2) - Math.Pow(l2, 2)) / (2 * l1 * Math.Sqrt(Math.Pow(corrd_x, 2) + Math.Pow(corrd_y, 2)))))/Math.PI;
+            }
+            if(teta2 > 0)
+            {
+                teta1 = 180 * ((double)Math.Atan2(corrd_y, corrd_x) - Math.Acos((Math.Pow(corrd_x, 2) + Math.Pow(corrd_y, 2) + Math.Pow(l1, 2) - Math.Pow(l2, 2)) / (2 * l1 * Math.Sqrt(Math.Pow(corrd_x, 2) + Math.Pow(corrd_y, 2))))) / Math.PI;
+            }
+
+            teta3 = angulo - (teta1 + teta2);
+        }
+
+        void retorna_angulos()
+        {
+            lb_t1.Text = teta1.ToString("F");
+            lb_t2.Text = teta2.ToString("F");
+            lb_t3.Text = teta3.ToString("F");
+        }
+
+    }
+}
